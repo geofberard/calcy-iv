1e02f0723270ff22256332b334a149b68e426638
Geoffrey Berard <geoffrey.berard@gmail.com>
2020-11-28 04:42:44 +0100

    Config - Persists in cookie

--- src/component/context/ConfigContext.tsx
+++ src/component/context/ConfigContext.tsx
@@ -1,19 +1,20 @@
 import * as React from "react";
 import { Config } from "../../data/Config";
 import { StateContext } from "./StateContext";
+import { saveConfigAsCookie, getConfigFromCookie } from "../../service/CookieService";
 
 const ConfigContext = React.createContext<StateContext<Config>>(null);
 
 export const ConfigProvider: React.FC = ({ children }) => {
-  // const [config, setConfig] = React.useState<Config>({
-  //   spreadsheetKey: "1ftOH6puWKaWCpcVzTb-1h_tVUcbji-WO6vQizNUmSuo",
-  //   pokemonSheet: "Sheet11",
-  //   pokedexSheet: "Sheet14",
-  // });
-  const [config, setConfig] = React.useState<Config>(null);
+  const [config, setConfig] = React.useState<Config>(getConfigFromCookie());
+
+  const saveConfig = (newConfig: Config) => {
+    setConfig(newConfig);
+    saveConfigAsCookie(newConfig);
+  };
 
   return (
-    <ConfigContext.Provider value={[config, setConfig]}>
+    <ConfigContext.Provider value={[config, saveConfig]}>
       {children}
     </ConfigContext.Provider>
   );
---
+++ src/service/CookieService.ts
@@ -0,0 +1,27 @@
+import { Config } from "../data/Config";
+
+const CONFIG_CNAME = "config";
+
+export const getConfigFromCookie = () => {
+  const name = `${CONFIG_CNAME}=`;
+  const decodedCookie = decodeURIComponent(document.cookie);
+  const ca = decodedCookie.split(";");
+  for (let i = 0; i < ca.length; i++) {
+    let c = ca[i];
+    while (c.charAt(0) === " ") {
+      c = c.substring(1);
+    }
+    if (c.indexOf(name) === 0) {
+      const newLocal = c.substring(name.length, c.length).split("§");
+      return {spreadsheetKey:newLocal[0], pokemonSheet:newLocal[1], pokedexSheet:newLocal[2]} as Config;
+    }
+  }
+  return null;
+};
+
+export const saveConfigAsCookie = (config: Config | undefined) => {
+  const d = new Date();
+  d.setTime(d.getTime() + (10 * 365 * 24 * 60 * 60 * 1000));
+  const cookieValue = `${config.spreadsheetKey}§${config.pokemonSheet}§${config.pokedexSheet}`;
+  document.cookie = `${CONFIG_CNAME}=${cookieValue};expires=${d.toUTCString()}`;
+};
